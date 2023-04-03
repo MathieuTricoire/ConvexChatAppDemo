6339848d35a839f55712f32f59a1022b5c420790
Mathieu Tricoire <mathieu@tricoi.re>
2023-04-03 12:02:56 +0200

    Initial Commit

---
+++ ConvexChatApp/ConvexChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvexChatAppApp.swift
+//  ConvexChatApp
+//
+//  Created by Mathieu Tricoire on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct ConvexChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
